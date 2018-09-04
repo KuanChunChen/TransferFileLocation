e2e79ba62b4ce33570326b73437ff3633635c502
Willly_Chen <willy_100600@castech.com.tw>
2018-09-04 13:57:47 +0800

    [180904][D] Separate MainActivity into different .Java

---
+++ app/src/main/java/utility/castles/getfile/Define.java
@@ -0,0 +1,27 @@
+package utility.castles.getfile;
+
+public class Define {
+
+    //file 相關
+    public final static String d_FILE_NAME="CTMSDebugLog";
+    public final static String d_DIR_PATH="/data/CTMS";
+    public final static String d_FILE_TYPE=".log";
+
+    public final static String d_FILE_TEST_STR ="testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+
+            "testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n"+"testeeeeettse\n";
+
+
+
+}
