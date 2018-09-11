b0ae16c374fa4a49baefa4c697fe3170b3ffb23e
Willly_Chen <willy_100600@castech.com.tw>
2018-09-11 14:12:02 +0800

    [180911][D]new feature-BottomNavigationView_fragment
    new bottomNavigation and fragment that can swtich
    page

--- app/src/main/java/utility/castles/getfile/TabFragment/FirstFragment.java
+++ app/src/main/java/utility/castles/getfile/TabFragment/FirstFragment.java
@@ -1,64 +1,46 @@
-package utility.castles.getfile;
-
+package utility.castles.getfile.TabFragment;
 
 import android.graphics.Color;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import utility.castles.getfile.GetfileMAin;
+import utility.castles.getfile.R;
 
 import static utility.castles.getfile.Define.d_FILE_NAME;
 import static utility.castles.getfile.Define.d_FILE_TEST_STR;
 
 
-public class MainActivity extends AppCompatActivity {
+public class FirstFragment extends Fragment {
 
-
-
-
+    private View view;
     public String[] strarrSplit;
-    //public ListAdapter mAdapter;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button btnStart =findViewById(R.id.btn1);
-        Button btnEnd =findViewById(R.id.btn2);
-        Button btnGetSize =findViewById(R.id.btn3);
-        Button btnDelFile =findViewById(R.id.btn4);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        Button btnStart,btnEnd,btnGetSize,btnDelFile;
+        view=inflater.inflate(R.layout.fragment_1,null);
+        btnStart=view.findViewById(R.id.frag1_btn1);
+        btnEnd=view.findViewById(R.id.frag1_btn2);
+        btnGetSize=view.findViewById(R.id.frag1_btn3);
+        btnDelFile=view.findViewById(R.id.frag1_btn4);
 
         final GetfileMAin getfile = new GetfileMAin() ;
-
         getfile.writeFile(d_FILE_NAME,d_FILE_TEST_STR);
 
-        //tVPath.setText(d_DIR_PATH+File.separator+d_FILE_NAME+".txt");
 
-
-        /* ** ListView ** */
-        //ListView mList = findViewById(R.id.listview1);
-        //mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,strarrSplit);
-        //mList.setAdapter(mAdapter);
-
-
-
-
-        /* **Start button** */
         btnStart.setOnClickListener(new Button.OnClickListener(){
 
             @Override
@@ -68,17 +50,17 @@ public class MainActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 String strReadData ="";
                 String strWriteData="";
-                TextView tVPath = findViewById(R.id.test1);
-                TextView tVStatus = findViewById(R.id.test2);
+                TextView tVPath = view.findViewById(R.id.frag1_text1);
+                TextView tVStatus = view.findViewById(R.id.frag1_text2);
                 tVStatus.setMovementMethod(ScrollingMovementMethod.getInstance());
 
                 strReadData=getfile.readFile(d_FILE_NAME);
                 //String[] strarrSplit = strReadData.split("\n");
-                if(strReadData!="FileNotFoundException"&&strReadData!="IOExecption"){
+                if(strReadData!="FileNotFoundException"&& strReadData!="IOExecption"){
 
 
                     //tVPath.setText("StoreDataPosition : "+d_DIR_PATH+File.separator+d_FILE_NAME+".log");
-                    tVPath.setText("StoreDataPosition : "+Environment.getExternalStorageDirectory().getAbsolutePath());
+                    tVPath.setText("StoreDataPosition : "+ Environment.getExternalStorageDirectory().getAbsolutePath());
                     tVPath.setTextColor(Color.WHITE);
                     tVPath.setBackgroundColor(android.graphics.Color.RED);
 
@@ -90,21 +72,26 @@ public class MainActivity extends AppCompatActivity {
                         strarrSplit = strReadData.split("\n");
 
 
-                        tVStatus.setText(strarrSplit[0]+strarrSplit[1]);
-                        tVStatus.setTextColor(Color.WHITE);
-                        tVStatus.setBackgroundColor(android.graphics.Color.BLUE);
+                        tVStatus.setText(strReadData);
+                        // tVStatus.setText(strarrSplit[0]+strarrSplit[1]);
+
 
                     }else{
                         //失敗下顯示錯誤情況
-                        tVStatus.setText(strWriteData);
+                        tVPath.setText(strWriteData);
+
                     }
                 }else{
-                    tVStatus.setText(strReadData);
+                    tVPath.setText(strReadData);
+
                 }
+                tVStatus.setTextColor(Color.WHITE);
+                tVStatus.setBackgroundColor(android.graphics.Color.BLUE);
 
             }
 
         });
+
 
 
 
@@ -115,18 +102,25 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
         /* **get Size button** */
         btnGetSize.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
                 String strSize;
-                TextView tVSize =findViewById(R.id.test1);
+                TextView tVSize =view.findViewById(R.id.frag1_text1);
+                TextView tClear =view.findViewById(R.id.frag1_text2);
                 strSize = getfile.getFileSize(d_FILE_NAME);
                 tVSize.setText("file size:"+strSize);
                 tVSize.setTextColor(Color.WHITE);
                 tVSize.setBackgroundColor(android.graphics.Color.RED);
+                tClear.setText("");
+                tClear.setBackgroundColor(Color.WHITE);
             }
         });
+
+
+
 
         /* **DelFile button** */
         btnDelFile.setOnClickListener(new Button.OnClickListener(){
@@ -135,27 +129,25 @@ public class MainActivity extends AppCompatActivity {
                 Boolean boDel;
 
 
-                TextView tVSize =findViewById(R.id.test1);
+                TextView tVSize =view.findViewById(R.id.frag1_text1);
+                TextView tClear =view.findViewById(R.id.frag1_text2);
                 boDel = getfile.delFile(d_FILE_NAME);
                 tVSize.setText("Delete file (boolean):"+boDel);
                 tVSize.setTextColor(Color.WHITE);
-                tVSize.setBackgroundColor(android.graphics.Color.RED);
+                tVSize.setBackgroundColor(Color.RED);
+                tClear.setText("");
+                tClear.setBackgroundColor(Color.WHITE);
             }
         });
 
+
+
+
+        return view;
     }
 
 
 
 
 
-
-
-
-
-
-
-
-
 }
-
---
+++ app/src/main/java/utility/castles/getfile/TabFragment/SecondFragment.java
@@ -0,0 +1,21 @@
+package utility.castles.getfile.TabFragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import utility.castles.getfile.R;
+
+public class SecondFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_2,null);
+    }
+}
